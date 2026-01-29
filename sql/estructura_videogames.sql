f6d45eeed28891d0aeb0b19466fe6e9066d5fefc
manufranso <manuelfrancessoler@gmail.com>
2026-01-29 14:08:53 +0100

    modificacion estructura sql

--- sql/estructura_videogames.sql
+++ sql/estructura_videogames.sql
@@ -1,70 +1,28 @@
+
+
 USE videogames;
 
--- TABLA METACRITIC
 
-ALTER TABLE metacritic
-MODIFY COLUMN title VARCHAR(255) NOT NULL;
-
-ALTER TABLE metacritic
-MODIFY COLUMN release_date DATE;
-
-ALTER TABLE metacritic
-MODIFY COLUMN meta_score INT;
-
-ALTER TABLE metacritic
-MODIFY COLUMN user_score INT;
-
-ALTER TABLE metacritic
-MODIFY COLUMN genre VARCHAR(255);
-
-ALTER TABLE metacritic
-ADD PRIMARY KEY (title);
+CREATE TABLE metacritic (
+    title VARCHAR(255) NOT NULL,
+    release_date DATE,
+    genre VARCHAR(255),
+    meta_score INT,
+    user_score INT,
+    PRIMARY KEY (title)
+);
 
 
--- TABLA SALES
+CREATE TABLE sales (
+    title VARCHAR(255) NOT NULL,
+    genre VARCHAR(255),
+    total_sales FLOAT,
+    na_sales FLOAT,
+    jp_sales FLOAT,
+    pal_sales FLOAT,
+    other_sales FLOAT,
+    sales_non_japan FLOAT,
+    PRIMARY KEY (title)
+);
 
- ALTER TABLE sales
- MODIFY COLUMN title VARCHAR(255) NOT NULL;
-
-ALTER TABLE sales
-MODIFY COLUMN genre VARCHAR(255);
-
-ALTER TABLE sales
-MODIFY COLUMN  total_sales FLOAT;
-
-ALTER TABLE sales
-MODIFY COLUMN  na_sales FLOAT;
-
-ALTER TABLE sales
-MODIFY COLUMN  jp_sales FLOAT;
-
-ALTER TABLE sales
-MODIFY COLUMN  pal_sales FLOAT;
-
-ALTER TABLE sales
-MODIFY COLUMN  other_sales FLOAT;
-
-ALTER TABLE sales
-MODIFY COLUMN  sales_non_japan FLOAT;
-
-ALTER TABLE sales
-ADD PRIMARY KEY (title);
-
-
--- Hemos decidido NO incluir la restricción de Foreign Key .
--- La razón es que hay juegos en la tabla 'sales' que no existen en 'metacritic'.
--- Si forzamos la FK, el script fallaría o tendríamos que borrar esas ventas.
---
--- Es mejor conservar los datos de ventas (aunque no tengan crítica) 
--- que perderlos por una regla estricta. Trabajaremos con LEFT JOIN.
-
--- Query para comprobar los juegos que dan conflicto :
--- SELECT s.title 
--- FROM sales s
--- LEFT JOIN metacritic m ON s.title = m.title
--- WHERE m.title IS NULL;
-
-
-
-
-
+select * from sales;
